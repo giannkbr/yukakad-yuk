0bb4749fa5efaa65f1c73f4275af2bc9db22f0df
vakbarrr <72488632+vakbarrr@users.noreply.github.com>
2021-09-02 11:02:42 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# yukakad-yuk
+ 
---
+++ application/models/ModelAuth.php
@@ -0,0 +1,16 @@
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+                        
+class ModelAuth extends CI_Model 
+{
+    public function select()
+    {
+
+    }                        
+                        
+}
+
+
+/* End of file ModelAuth_model.php and path /application/models/ModelAuth_model.php */
+
