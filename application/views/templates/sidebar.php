606d26255f0e131d67ed32a0d5bfcd153e58cb6b
vakbarrr <72488632+vakbarrr@users.noreply.github.com>
2021-09-02 11:40:54 +0700

    Update sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -17,27 +17,89 @@
 				<a href="#" class="nav-link">
 					<i class="nav-icon fas fa-archway"></i>
 					<p>
-						CMS
+						Data
 						<i class="right fas fa-angle-left"></i>
 					</p>
 				</a>
 				<ul class="nav nav-treeview">
 					<li class="nav-item">
-						<a href="./index.html" class="nav-link">
+						<a href="#" class="nav-link">
 							<i class="far fa-circle nav-icon"></i>
 							<p>Hero</p>
 						</a>
 					</li>
-				</ul>
+					<li class="nav-item">
+						<a href="#" class="nav-link">
+							<i class="far fa-circle nav-icon"></i>
+							<p>Features</p>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link">
+							<i class="far fa-circle nav-icon"></i>
+							<p>Demos</p>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a href="#" class="nav-link">
+							<i class="far fa-circle nav-icon"></i>
+							<p>Packet</p>
+						</a>
+					</li>
 			</li>
 			<li class="nav-item">
 				<a href="#" class="nav-link">
-					<i class="nav-icon fas fa-sign-out-alt"></i>
-					<p>
-						Logout
-					</p>
+					<i class="far fa-circle nav-icon"></i>
+					<p>Testimoni</p>
 				</a>
 			</li>
+			<li class="nav-item">
+				<a href="#" class="nav-link">
+					<i class="far fa-circle nav-icon"></i>
+					<p>Payment Process</p>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a href="#" class="nav-link">
+					<i class="far fa-circle nav-icon"></i>
+					<p>Blog</p>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a href="#" class="nav-link">
+					<i class="far fa-circle nav-icon"></i>
+					<p>Contact</p>
+				</a>
+			</li>
+		</ul>
+		</li>
+
+		<li class="nav-item">
+			<a href="#" class="nav-link">
+				<i class="nav-icon fas fa-cogs"></i>
+				<p>
+					Settings
+					<i class="right fas fa-angle-left"></i>
+				</p>
+			</a>
+			<ul class="nav nav-treeview">
+				<li class="nav-item">
+					<a href="#" class="nav-link">
+						<i class="far fa-circle nav-icon"></i>
+						<p>Users</p>
+					</a>
+				</li>
+			</ul>
+		</li>
+
+		<li class="nav-item">
+			<a href="#" class="nav-link">
+				<i class="nav-icon fas fa-sign-out-alt"></i>
+				<p>
+					Logout
+				</p>
+			</a>
+		</li>
 		</ul>
 	</nav>
 	<!-- /.sidebar-menu -->
